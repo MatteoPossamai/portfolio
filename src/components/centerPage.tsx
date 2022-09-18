44405523612c227172e63b7d1d8853289e00866f
MatteoPossamai <mpossamaim@gmail.com>
2022-09-18 16:19:37 +0200

    page center component

---
+++ src/components/centerPage.tsx
@@ -0,0 +1,9 @@
+function CenterPage() {
+    return (
+        <div className="center-page" style={{"border": "1px solid black"}}>
+            <h1>Center Page</h1>
+        </div>
+    );
+    }
+
+export default CenterPage;
