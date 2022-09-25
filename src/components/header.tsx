3046e41989d606fcec812a25fdae0a0def86f13f
MatteoPossamai <mpossamaim@gmail.com>
2022-09-25 18:35:02 +0200

    handled the media query that opens the menu lateral and the hamburger menu. Now WHen the screen is small it shows up the lateral menu with the hamburger menu itself. Not bad so far. Now need to compolete the other parts of the content itself. Then the list online to check

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,6 +6,8 @@ function Header() {
     const [scrollY, setScrollY] : [number, Dispatch<SetStateAction<number>>]= React.useState(0);
     const [agileClasses, setAgileClasses] : [string, Dispatch<SetStateAction<string>>] = React.useState("header");
 
+    const [openMenu, setOpenMenu] : [boolean, Dispatch<SetStateAction<boolean>>] = React.useState(true);
+
     window.onscroll = () => {
         if(scrollY < window.scrollY){
             setIsSticky(false);
@@ -33,18 +35,37 @@ function Header() {
     }, [isOnTop, isSticky]);
 
     return (
-        <div className={agileClasses}>
+        <>
+            <div className={agileClasses}>
 
-            <a href="/" className="resume-button">Resume</a>
-            
-            <div className="header-text">
-                <a href="#about" className="header-button"><p className="number">01. </p> About</a>
-                <a href="/" className="header-button"><p className="number">02. </p> Job experience</a>
-                <a href="/" className="header-button"><p className="number">03. </p> Projects</a>
-                <a href="/" className="header-button"><p className="number">04. </p> Open Source</a>
-                <a href="/" className="header-button"><p className="number">05. </p> Contact</a>
+                <a href="/" className="resume-button">Resume</a>
+                
+                <div className="header-text">
+                    <a href="#about" className="header-button"><p className="number">01. </p> About</a>
+                    <a href="/" className="header-button"><p className="number">02. </p> Job experience</a>
+                    <a href="/" className="header-button"><p className="number">03. </p> Projects</a>
+                    <a href="/" className="header-button"><p className="number">04. </p> Open Source</a>
+                    <a href="/" className="header-button"><p className="number">05. </p> Contact</a>
+                </div>
+                <svg className="hamburger" viewBox="0 0 100 80" width="40" height="40" onClick={() => {setOpenMenu(!openMenu)}}>
+                    <rect width="100" height="20" rx="10" className={openMenu ? "visible":"invisible"}></rect>
+                    <rect y="35" width="100" height="20" rx="10" className={"visible"} id={openMenu ? "" :"right"}></rect>
+                    <rect y="70" width="100" height="20" rx="10" className={openMenu ? "visible":"invisible"}></rect>
+                    <rect y="35" width="100" height="20" rx="10" className={openMenu ? "invisible":"visible"} id={openMenu ? "" :"left"}></rect>
+                </svg>
             </div>
-        </div>
+
+            <div className={openMenu ? "close" : "open"}>
+                <div className="menu-text">
+                    <h1 className="menuhead">  Menu</h1>
+                    <a href="#about" onClick={() =>{setOpenMenu(!openMenu)}} className="menu-button"><p className="number">01. </p> About</a>
+                    <a href="/" onClick={() =>{setOpenMenu(!openMenu)}} className="menu-button"><p className="number">02. </p> Job experience</a>
+                    <a href="/" onClick={() =>{setOpenMenu(!openMenu)}} className="menu-button"><p className="number">03. </p> Projects</a>
+                    <a href="/" onClick={() =>{setOpenMenu(!openMenu)}} className="menu-button"><p className="number">04. </p> Open Source</a>
+                    <a href="/" onClick={() =>{setOpenMenu(!openMenu)}} className="menu-button"><p className="number">05. </p> Contact</a>
+                </div>
+            </div>
+        </>
     );
 }
 
