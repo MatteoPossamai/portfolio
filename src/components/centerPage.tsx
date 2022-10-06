cb55e21f7a501976262938489288cbf09b38e791
poss03251 <mpossamaim@gmail.com>
2022-10-06 09:42:32 +0200

    Fixed the problem with sovrapposition of the div and the mail in the first part of the page, and handled the margin top problem between the job experience and about

--- src/components/centerPage.tsx
+++ src/components/centerPage.tsx
@@ -4,8 +4,8 @@ function CenterPage() {
             <p className="first-line">Welcome, I am</p>
             <h1 className="name">Matteo Possamai.</h1>
             <h1 className="short-description">I build Softwares in the Web</h1>
-            <p className="long-description">I am a young Software Engineer that designs and builds performant <br />
-            solutions for the Web and not only. Currently I am studying Computer <br />
+            <p className="long-description">I am a young Software Engineer that designs and builds performant 
+            solutions for the Web and not only. Currently I am studying Computer 
             Science to enhance my skills and knowledge in the domain</p>
         </div>
     );
