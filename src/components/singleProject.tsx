e618a193a36f7614ee39605eedb92d91af3d9be3
poss03251 <mpossamaim@gmail.com>
2022-10-06 16:21:05 +0200

    staretd the single project page, adding the icon on top, and fixing the icons in the txs file, making more reactive the text parsing. Getting stuck with an error of displaying images from the base folder

--- src/components/singleProject.tsx
+++ src/components/singleProject.tsx
@@ -1,18 +1,25 @@
 import {useParams} from 'react-router-dom';
 import FourOFour from './404';
-
+import projects from "../data/projects.json";
+import svg_component from '../data/svg';
 
 function SingleProject(){
-    const validIds = ['mad4science', 'moondev', 'referendum', 'portfolio'];
+    const validIds = ['mad4', 'moondev', 'referendum'];
 
     const id = useParams<{id:string}>();
+    
     const valor:any = id.id;
+    let project = projects['projects'].filter((project) => project['id'] === valor)[0];
+    let image = project['id'] + '.png';
+    console.log(image)
     if (!validIds.includes(valor)){
         return (<FourOFour />)
     }
     return (
         <div className='code404'>
-            <h1>{valor}</h1>
+            <h1>{project['project-name']}</h1>
+            <div className='topIcon'>{svg_component[project['id']]}</div>
+            <img src={`${image}`} alt={project['project-name']} />
         </div>
     )
 }
