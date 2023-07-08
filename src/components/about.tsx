6db9d9a80e9b0f9a74a102830dba58e4ccb5f40c
poss03251 <mpossamaim@gmail.com>
2023-07-08 18:43:32 +0200

    first content update 2023

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,16 +6,18 @@ function AboutPage() {
                 <hr className="header-line"/>
             </header>
             <div>
-            <p className="long-description bold">Hi, my name is Matteo. I started
-                                                programming and developing my own code in 2018, when I
-                                                fall in love with static website creation in
+            <p className="long-description bold">Hi, my name is Matteo. I started programming 2018, when I
+                                                fall in love with website creation in
                                                 HTML / CSS / Javascript.  </p>
 
             <p className="long-description bold">Then I moved to backend development, and I made an
                                                 internship as Backend Engineer in <a href="https://zwap.in/">ZWAP.in</a>, 
-                                                where I developed, maintained and tested their 
-                                                backend system in Django. while currently I am enrolled in my 
-                                                Computer Science degree, trying to become a much better developer. </p>
+                                                where I developed their backend system in Django, while I was stating my
+                                                Computer Science degree </p>
+
+            <p className="long-description bold">At the moment, I am doing my Internship as an SDE in Amazon Web
+                                                Services, in the IDE team, developing new integrations and 
+                                                features for the online platform. </p>
 
             <p className="long-description bold">Here there are the most important technologies I worked 
                                                 with recently and I am the most confident with:</p>
@@ -28,7 +30,7 @@ function AboutPage() {
                 <ul className="tech-list">
                     <li><span>React</span></li>
                     <li><span>Golang</span></li>
-                    <li><span>C++</span></li>
+                    <li><span>Java</span></li>
                 </ul>
             </div>
             </div>
--- src/components/centerPage.tsx
+++ src/components/centerPage.tsx
@@ -3,10 +3,14 @@ function CenterPage() {
         <div className="center-page" >
             <p className="first-line">Welcome, I am</p>
             <h1 className="name">Matteo Possamai.</h1>
-            <h1 className="short-description">I build Softwares in the Web</h1>
+            <h1 className="short-description">I build Softwares in the Web, and in the Cloud</h1>
             <p className="long-description">I am a young Software Engineer that designs and builds performant 
-            solutions for the Web and not only. Currently I am studying Computer 
-            Science to enhance my skills and knowledge in the domain</p>
+            solutions for the Web and the Cloud. 
+
+            <br />I am a SDE intern in Amazon Web Services, working in the IDEs team. I am also a student at the University of Trento, 
+            in the Computer Science Bachelor's Degree.
+            
+            </p>
         </div>
     );
     }
